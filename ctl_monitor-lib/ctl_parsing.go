ff33bf933e0f1e09005913127f47441c2e343d08
rmbellovin <r.m.bellovin@gmail.com>
2019-09-21 22:13:09 +0100

    reorganized library files

---
+++ ctl_monitor-lib/ctl_parsing.go
@@ -0,0 +1,242 @@
+package ctl_monitor_lib
+
+
+import "crypto/x509"
+import "net/http"
+import "log"
+import "io/ioutil"
+import "encoding/json"
+import "strconv"
+import "encoding/base64"
+import "encoding/binary"
+import "errors"
+
+var GET_STH string = "ct/v1/get-sth"
+var GET_ENTRIES string = "ct/v1/get-entries"
+var LOG_ENTRY_TYPE_MAP map[uint16]string = map[uint16]string{0: "X509", 1: "PreCert"}
+
+type Signed_tree_head struct {
+    Tree_size uint64
+    Timestamp uint64
+    Sha256_root_hash string
+    Tree_head_signature string
+}
+
+type Raw_entry struct {
+    Leaf_input string
+    Extra_data string
+}
+
+type get_entry_response struct {
+    Entries []Raw_entry
+}
+
+type Merkle_tree_leaf struct {
+    Version uint8
+    MerkleLeafType uint8
+    Timestamp uint64
+    LogEntryType uint16
+    Entry string
+    Extra_data string
+}
+
+type certificate struct {
+    Length uint32
+    CertData []byte
+}
+
+func three_byte_to_uint32(bytes []byte) uint32 {
+    padded_length := make([]byte,4)
+    copy(padded_length[1:],bytes[:3])
+    return binary.BigEndian.Uint32(padded_length)
+
+}
+
+// get entries from ctl_host between start and end (inclusive).  throws a fatal error if called with an invalid CT log url, or for an invalid range, or if there is a network problem
+func Get_entries(ctl_host string, start uint64, end uint64) []Raw_entry {
+
+    if start < 0 || end < 0 {
+        log.Fatalln("Invalid range: start and end must be at least 0")
+    }
+    if start > end {
+        log.Fatalln("Invalid range: start must be at most end")
+    }
+
+    req, err := http.NewRequest("GET", ctl_host + GET_ENTRIES, nil)
+    if err != nil {
+	log.Fatalln(err)
+    }
+
+// initialize an empty list of entries retrieved
+    var all_entries_received []Raw_entry
+
+// the RFC allows CT logs to only return a few entries at a time, so we keep making requests
+    for start <= end {
+
+// the url to request entries between START and END (inclusive) is https://<log server>/ct/v1/get-entries?start=START&end=END, so we build it with req.URL.Query()
+        q := req.URL.Query()
+        q.Add("start", strconv.FormatUint(start,10))
+        q.Add("end", strconv.FormatUint(end,10))
+        req.URL.RawQuery = q.Encode()
+
+        resp, err := http.Get(req.URL.String())
+        if err != nil {
+            log.Fatalln(err)
+        }
+        defer resp.Body.Close()
+
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+	    log.Fatalln(err)
+        }
+
+// for some reason, go won't unmarshal data into an array of structs, but will unmarshal data into an auxiliary struct whose data is an array of structs
+        var entry_array get_entry_response
+        err = json.Unmarshal(body, &entry_array)
+        if err != nil {
+            log.Fatalln("Get_entries", start, end, err)
+        }
+
+// append entries received to our list
+        all_entries_received = append(all_entries_received, entry_array.Entries...)
+
+        start += uint64(len(entry_array.Entries))
+    }
+
+    return all_entries_received
+
+}
+
+
+// get the signed tree head.  throws a fatal error if the CT log url is invalid, or there is a network issue
+func Get_sth(ctl_host string) (Signed_tree_head, error) {
+    
+    var sth Signed_tree_head
+
+    resp, err := http.Get(ctl_host + GET_STH)
+    if err != nil {
+	return sth, err
+    }
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+	return sth, err
+    }
+
+// use json to unmarshal the response into the appropriate form
+    err = json.Unmarshal(body,&sth)
+    if err != nil {
+	return sth, err
+    }
+
+    return sth, nil
+
+}
+
+// take an entry and parse it as a MerkleTreeLeaf.  it's base64-encoded, so decode and parse "by hand".  the Merkle_tree_leaf.Entry field will require further processing
+func Parse_leaf_input(entry Raw_entry) (Merkle_tree_leaf, error) {
+
+    var leaf Merkle_tree_leaf
+
+    leaf_input := entry.Leaf_input
+
+    binary_decode, err := base64.StdEncoding.DecodeString(leaf_input)
+    if err != nil {
+	log.Println(err)
+        return leaf, err
+    }
+
+    if len(binary_decode) < 12 {
+        err = errors.New("Invalid entry")
+        return leaf, err
+    }
+
+// the first byte is the version of the protocol. should be 0
+    leaf.Version = binary_decode[0]
+// the second byte gives the Merkle leaf type. should be 0
+    leaf.MerkleLeafType = binary_decode[1]
+// the next 8 bytes are a timestamp, in milliseconds
+    leaf.Timestamp = binary.BigEndian.Uint64(binary_decode[2:10])
+// the next 2 bytes give the log entry type. should be 0 or 1
+    leaf.LogEntryType = binary.BigEndian.Uint16(binary_decode[10:12])
+// the remainder of the leaf_input is the actual entry
+    leaf.Entry = base64.StdEncoding.EncodeToString(binary_decode[12:])
+
+// this is the extra data field from the raw entry
+    leaf.Extra_data = entry.Extra_data
+
+    return leaf, nil
+
+}
+
+// the Merkle_tree_leaf.Entry field is a base64 string; the first 4 characters (decoding to 3 bytes) give the length, and the rest is the certificate itself.  there seems to be some auxiliary padding at the end which has to be stripped out.  this returns a certificate, which consists of the length, plus the DER-encoded certificate itself
+func Parse_cert_entry(leaf Merkle_tree_leaf) (certificate, error) {
+
+    var cert_bytes []byte
+
+    var cert certificate
+
+    switch leaf.LogEntryType {
+    case 0: 
+// we have a normal x509 entry, with 3 bytes at the start giving the length
+        binary_decode, err := base64.StdEncoding.DecodeString(leaf.Entry)
+        if err != nil {
+            log.Println(err)
+	    return cert, err
+        }
+        if len(binary_decode)<3 {
+            err = errors.New("Invalid certificate: too short")
+            return cert, err
+        }
+        cert_bytes = binary_decode
+        
+    case 1:
+// we have a precert entry; there are 32 bytes at the start of the extra_data field giving the length
+        binary_decode, err := base64.StdEncoding.DecodeString(leaf.Extra_data)
+        if err != nil {
+	    log.Println(err)
+            return cert, err
+        }
+        if len(binary_decode)<3 {
+            err = errors.New("Invalid certificate entry: too short")
+            return cert, err
+        }
+        cert_bytes = binary_decode
+
+    default:
+        err := errors.New("Unknown LogEntryType")
+        return cert, err
+    }
+
+    cert.Length = three_byte_to_uint32(cert_bytes[0:3])
+
+    if uint32(len(cert_bytes)) < cert.Length+3 {
+        err := errors.New("Invalid certificate: too short")
+        return cert, err
+    }
+// the actual certificate seems to be longer than it should be
+    cert.CertData = cert_bytes[3:cert.Length+3]
+
+    return cert, nil
+
+}
+
+// parse the DER-encoded byte sequence, and extract the commonname field.  returns the error of either parsing the leaf.Entry/leaf.Extra_data field, or of parsing the DER-encoded bytes
+func Get_commonname(leaf_input Merkle_tree_leaf) (string, error) {
+
+    cert, err := Parse_cert_entry(leaf_input)
+    if err != nil {
+        log.Println(err)
+        return "", err
+    }
+    asn_der := cert.CertData
+    decoded_cert, err2 := x509.ParseCertificate(asn_der)
+    if err != nil {
+        log.Println(err)
+        return "", err2
+    }
+
+    return decoded_cert.Subject.CommonName, nil
+
+}
---
+++ ctl_monitor-lib/monitor.go
@@ -0,0 +1,425 @@
+package ctl_monitor_lib
+
+import "fmt"
+import "log"
+import "database/sql"
+import _ "github.com/mattn/go-sqlite3"
+import "time"
+import "regexp"
+//import "../ctl_parsing"
+import "strings"
+import "github.com/prometheus/client_golang/prometheus"
+
+var REQUEST_SIZE uint64 = 1024
+var SLEEP time.Duration = 5 * time.Minute
+
+type db_row struct {
+    timestamp uint64
+    common_name string
+    cert string
+    logentrytype string
+}
+
+type Monitor struct {
+    ctl_host string
+    hostnames []string
+    tree_head Signed_tree_head
+    database *sql.DB
+    certificate_metrics *prometheus.CounterVec
+    Signal chan int
+    VERBOSE bool
+    NON_STRICT bool
+}
+
+func NewMonitor(ctl_host string, hostnames []string, verbose bool, no_delete bool, non_strict bool) (*Monitor, error) {
+
+    var monitor Monitor
+
+    monitor.VERBOSE = verbose
+
+    if monitor.VERBOSE { fmt.Printf("Initializing new CTL monitor.\n") }
+
+    monitor.ctl_host = ctl_host
+    if monitor.VERBOSE { fmt.Printf("Certificate transparency log: %s\n", monitor.ctl_host) }
+    monitor.hostnames = hostnames
+    if monitor.VERBOSE { fmt.Printf("Hostnames: \n%v\n", hostnames) }
+
+    monitor.Signal = make(chan int)
+
+    sth, err := Get_sth(ctl_host)
+    if err != nil {
+        log.Println("Error getting signed tree head.")
+        return &monitor, err
+    }
+    monitor.tree_head = sth
+    if monitor.VERBOSE { fmt.Printf("Tree head: \n%v\n", monitor.tree_head) }
+
+// prepare database
+    database_name := make_db_name(ctl_host)
+    monitor.database, err = prepare_db(database_name, monitor.VERBOSE, no_delete)
+    if err != nil {
+        log.Println("Error initializing database.")
+        return &monitor, err
+    }
+
+// prepare metrics
+    monitor.certificate_metrics = prepare_metrics(monitor.hostnames)
+    prometheus.MustRegister(monitor.certificate_metrics)
+
+    monitor.NON_STRICT = non_strict
+
+    return &monitor, err
+
+}
+
+func (m *Monitor) CTL_host() string {
+
+    return m.ctl_host
+
+}
+
+// add hostnames
+func (m *Monitor) Add_hostnames(new_hostnames []string) {
+
+    for _, entry := range new_hostnames {
+// if a new hostname isn't already in the hostname list, append it
+        if index(m.hostnames, entry) == -1 {
+            m.hostnames = append(m.hostnames, entry)
+        }
+        
+// create new counters for the new hostname, one for X509 entries, one for PreCert entries
+        m.certificate_metrics.WithLabelValues(entry, "X509")
+        m.certificate_metrics.WithLabelValues(entry, "PreCert")
+    }
+
+
+    if m.VERBOSE { fmt.Printf("Hostname list:%v\n", m.hostnames) }
+
+}
+
+// remove hostname
+func (m *Monitor) Remove_hostname(hostname string) {
+
+    if m.VERBOSE { fmt.Printf("Removing %s from hostnames\n", hostname) }
+
+    for i, entry := range m.hostnames {
+        if entry == hostname {
+            m.hostnames = append(m.hostnames[:i],m.hostnames[i+1:]...)
+        }
+    }
+    
+    if m.VERBOSE { fmt.Println("Hostname list:\n", m.hostnames) }
+
+}
+
+// list hostnames
+func (m *Monitor) Get_hostnames() []string {
+
+    return m.hostnames
+
+}
+
+// return tree head
+func (m *Monitor) Get_tree_head() Signed_tree_head {
+
+    return m.tree_head
+
+}
+
+// return timestamp of treehead
+func (m *Monitor) Get_timestamp() uint64 {
+    
+    return m.tree_head.Timestamp
+
+}
+
+// return treesize
+func (m *Monitor) Get_treesize() uint64 {
+
+    return m.tree_head.Tree_size
+
+}
+
+// get timestamps and certificates for specified hostname
+func (m *Monitor) List_certs(hostname string) []db_row {
+
+// query the database
+    rows, err := m.database.Query("SELECT DISTINCT timestamp, certificate, logentrytype FROM certificates WHERE commonname = ?", hostname)
+    if err != nil {
+        log.Println("Error accessing database.")
+        log.Fatalln(err)
+    }
+    defer rows.Close()
+
+// scan through the results and append each one to a list of results
+    var results []db_row
+    var row db_row
+    for rows.Next() {
+        err = rows.Scan(&row.timestamp, &row.cert, &row.logentrytype)
+        if err != nil {
+            log.Println("Error accessing database row.")
+            log.Fatalln(err)
+        }
+        results = append(results, row)
+    }
+
+// return results
+    return results
+
+}
+
+// search entire ct log and build database
+func (m *Monitor) Build_database() {
+
+// add all entries
+    if m.VERBOSE { fmt.Printf("Building database of certificates for hostnames %v\n", m.hostnames) }
+    m.add_entries(0, m.tree_head.Tree_size-1)
+
+}
+
+// search ct log from entry 'start' to entry 'end' and add the appropriate certificates to the database
+func (m *Monitor) add_entries(start uint64, end uint64) {
+
+// prepare a statement to insert results into the database
+    if m.VERBOSE { fmt.Printf("Searching %s between entries %d and %d for certificates for hostnames %v\n", m.ctl_host, start, end, m.hostnames) }
+    statement, err := m.database.Prepare("INSERT OR IGNORE INTO certificates (timestamp, commonname, certificate, logentrytype) VALUES (?, ?, ?, ?)")
+    if err != nil {
+        log.Fatalln(err)
+    }
+    defer statement.Close()
+
+    var entries []Raw_entry
+    var leaf Merkle_tree_leaf
+    var timestamp uint64
+    var common_name string
+// make sure we don't go past the end of the CT log
+    max := min(m.tree_head.Tree_size - 1, end)
+
+// certificate logs are big; only fetch a few entries at a time
+    for ; start <= max; start += REQUEST_SIZE {
+        if m.VERBOSE { fmt.Printf("Checking entries starting at %d\n", start) }
+// request at most REQUEST_SIZE entries from the CT log
+        finish := min(start+REQUEST_SIZE, max)
+        entries = Get_entries(m.ctl_host, start, finish)
+
+// parse each entry the CT log returned
+        for _, entry := range entries {
+// if the entry is malformed, skip it and go on to the next one
+            leaf, err = Parse_leaf_input(entry)
+            if err != nil {
+                log.Println(err)
+                continue
+            }
+            timestamp = leaf.Timestamp
+
+// if the LogEntryType is neither 0 (X509) nor 1 (PreCert), skip it and go on to the next entry
+            if leaf.LogEntryType != 0 && leaf.LogEntryType != 1 {
+                continue
+            }
+
+// parse the certificate entry and extract the commonname field.  if it's malformed, skip it and go on to the next entry
+            common_name, err = Get_commonname(leaf)
+            if err != nil {
+                log.Println(err)
+                continue
+            }
+            if m.VERBOSE { fmt.Println(timestamp, common_name, LOG_ENTRY_TYPE_MAP[leaf.LogEntryType]) }
+
+// check whether the commonname is one of the hostnames we're monitoring for
+            var hostname string
+            var i int
+            if m.NON_STRICT {
+                hostname, i = index_non_strict(m.hostnames, common_name)
+            } else {
+                hostname = common_name
+                i = index(m.hostnames, common_name)
+            }
+
+// if it is, add the timestamp, certificate type, commonname, and certificate type to the database, and increment the appropriate metric
+            if i != -1 {
+                if m.VERBOSE { fmt.Println("Adding", timestamp, common_name, leaf.Entry, LOG_ENTRY_TYPE_MAP[leaf.LogEntryType]) }
+                results, _ := statement.Exec(timestamp, common_name, leaf.Entry, LOG_ENTRY_TYPE_MAP[leaf.LogEntryType])
+                rows_added, _ := results.RowsAffected()
+
+                metric := m.certificate_metrics.WithLabelValues(hostname, LOG_ENTRY_TYPE_MAP[leaf.LogEntryType])
+                metric.Add(float64(rows_added))
+            }        
+        }
+    }
+
+    if m.VERBOSE { fmt.Printf("Done searching %s for certificates for hostnames %v\n", m.ctl_host, m.hostnames) }
+
+}
+
+// wakes up every SLEEP minutes to check for new entries.  
+func (m *Monitor) Activate(signal chan int) {
+
+    if m.VERBOSE { fmt.Printf("Monitoring certificate transparency log %s for certificates for the following hostnames:\n%v\n", m.ctl_host, m.hostnames) }
+    loop:
+    for true {
+        
+        m.Check()
+
+// function exits if it receives a signal from the Stop() function
+        select {
+            case <- signal: break loop
+            case <- time.After(SLEEP): if m.VERBOSE { fmt.Printf("Waking up\n") }
+        }
+    }
+
+}
+
+// check for new certificates
+func (m *Monitor) Check() {
+
+// get the new signed tree head; if there's a problem, print and error and return
+    new_sth, err := Get_sth(m.ctl_host)
+    if err != nil {
+        log.Println("Error getting a new signed tree head")
+        log.Println(err)
+        return
+    }
+
+    if new_sth.Tree_size != m.tree_head.Tree_size {
+        if m.VERBOSE { fmt.Printf("New entries found; %s now contains %d entries\n", m.ctl_host, new_sth.Tree_size) }
+
+        m.add_entries(m.tree_head.Tree_size, new_sth.Tree_size-1)
+
+        m.tree_head = new_sth
+    }
+
+}
+
+// stop automatically checking for new entries every SLEEP
+func (m *Monitor) Stop(signal chan int) {
+
+    if m.VERBOSE { fmt.Printf("No longer actively monitoring %s\n", m.ctl_host) }
+    signal <- 1
+
+}
+
+// deletes database entries for specified hostname
+func (m *Monitor) Delete_db_entries(hostname string) {
+
+    if m.VERBOSE { fmt.Printf("Deleting database entries for hostname %s.", hostname) }
+// prepare a statement to delete entries from the database
+    statement, err := m.database.Prepare("DELETE FROM certificates WHERE commonname = ?")
+    if err != nil {
+        log.Println("Database error while attempting to delete entries for hostname " + hostname)
+        log.Println(err)
+        return
+    }
+    statement.Exec(hostname)
+    statement.Close()
+
+    m.certificate_metrics.DeleteLabelValues(hostname, "X509")
+    m.certificate_metrics.DeleteLabelValues(hostname, "PreCert")
+
+}
+
+// make a database name out of ctl_host
+func make_db_name(ctl_host string) string {
+
+    name := ctl_host + ".db"
+
+    re := regexp.MustCompile(`http.://`)
+    name = re.ReplaceAllString(name, "")
+    re = regexp.MustCompile(`/`)
+    name = re.ReplaceAllString(name, ".")
+    re = regexp.MustCompile(`\.+`)
+    name = re.ReplaceAllString(name, ".")
+
+    name = "./" + name
+
+    return name
+
+}
+
+// finds the index of the first occurence of 'word' as a superstring in an array of strings, and returns the substring and the index.  returns "", -1 if word does not appear in array
+func index_non_strict(array []string, word string) (string, int) {
+
+    for i, entry := range array {
+        if strings.Contains(word, entry) {
+            return entry, i
+        }
+    }
+
+    return "", -1
+
+}
+
+// finds the index of the first occurence of 'word' in an array of strings.  returns -1 if word does not appear in array
+func index(array []string, word string) int {
+
+    for i, entry := range array {
+        if word == entry {
+            return i
+        }
+    }
+
+    return -1
+
+}
+
+// min of two uint64's
+func min(a uint64, b uint64) uint64 {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+// prepare database
+func prepare_db(database_name string, verbose bool, no_delete bool) (*sql.DB, error) {
+
+    db, err := sql.Open("sqlite3", database_name)
+    if err != nil {
+        log.Fatalln(err)
+        return db, err
+    }
+
+    if verbose { fmt.Printf("Results stored in sqlite3 database %s\n", database_name) }
+
+// clear the database if it wasn't empty, unless no_delete is set
+    if !no_delete {
+        statement, _ := db.Prepare("DROP TABLE IF EXISTS certificates")
+        statement.Exec()
+        statement.Close()
+    }
+
+// create a table with columns 'timestamp', 'commonname', 'certificate', 'logentrytype', and require each row to be unique
+    statement, _ := db.Prepare("CREATE TABLE IF NOT EXISTS certificates (timestamp INTEGER, commonname TEXT, certificate TEXT, logentrytype TEXT, PRIMARY KEY (timestamp, commonname, certificate, logentrytype) )")
+    statement.Exec()
+    statement.Close()
+
+    if verbose { fmt.Println("Table 'certificates' created with columns 'timestamp', 'commonname', 'certificate', and 'logentrytype'") }
+
+// delete any rows from the new table.  this shouldn't do anything
+    if !no_delete {
+        statement, _ := db.Prepare("DELETE FROM certificates")
+        statement.Exec()
+        statement.Close()
+        if verbose { fmt.Println("Table 'certificates' now empty") } 
+    }
+
+    return db, err
+
+}
+
+// prepare metrics
+func prepare_metrics(hostnames []string) *prometheus.CounterVec {
+
+// initialize a vector of counters, indexed by hostname and log entry type
+    countervec := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "certificate_metric",
+		Help: "Counts certificates added to the database.",
+	}, []string{"hostname", "log_entry_type"})
+
+    for _, entry := range hostnames {
+        countervec.WithLabelValues(entry, "X509")
+        countervec.WithLabelValues(entry, "PreCert")
+    }
+
+    return countervec
+
+}
